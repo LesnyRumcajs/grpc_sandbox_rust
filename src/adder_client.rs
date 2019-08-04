90af596efda8b9d6cd44d5d007e17ee0cd1d2f3b
Hubert Bugaj <hubert.bugaj@nexthink.com>
2019-08-04 22:12:31 +0200

    custom input

--- src/adder_client.rs
+++ src/adder_client.rs
@@ -8,21 +8,29 @@ use protos::adder_grpc::AdderClient;
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
-    if args.len() != 2 {
-        panic!("Expected exactly one argument, the port to connect to.")
+    if args.len() != 4 {
+        panic!("Expected exactly three arguments, the port to connect to and two numbers to add")
     }
     let port = args[1]
         .parse::<u16>()
         .unwrap_or_else(|_| panic!("{} is not a valid port number", args[1]));
+
+    let num1 = args[2]
+        .parse::<i32>()
+        .unwrap_or_else(|_| panic!("{} is not a valid number", args[1]));
+
+    let num2 = args[3]
+        .parse::<i32>()
+        .unwrap_or_else(|_| panic!("{} is not a valid number", args[1]));
 
     let env = Arc::new(EnvBuilder::new().build());
     let ch = ChannelBuilder::new(env).connect(format!("localhost:{}", port).as_str());
     let client = AdderClient::new(ch);
 
     let mut add_req = AddRequest::new();
-    add_req.set_first(2);
-    add_req.set_second(2);
+    add_req.set_first(num1);
+    add_req.set_second(num2);
 
     let check = client.add_numbers(&add_req).expect("RPC Failed!");
-    println!("2 + 2 = {}", check.get_result());
+    println!("{} + {} = {}", num1, num2, check.get_result());
 }
